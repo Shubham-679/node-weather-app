b86a38cec6015acffc7cccce6fce0838dc67007b
ShuBham <kushwahshubham679@gmail.com>
2020-08-23 15:01:59 +0530

    heroku setup

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 const path = require('path')
 const hbs = require('hbs')
 const request = require('postman-request');
@@ -102,4 +102,4 @@ app.get('*',(req,res)=>{
 })
 
 
-app.listen(port, () => console.log(`Example app listening on port port!`))
+app.listen(port, () => console.log(`Example app listening on port`+port))
